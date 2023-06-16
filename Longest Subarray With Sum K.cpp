35fbcaaca2d83641c9ec65328646cb1b6de77d94
Abhishek Garg <workwithabhigarg@gmail.com>
2023-06-16 15:27:02 +0530

    Most Optimal Approach Completed

--- Longest Subarray With Sum K.cpp
+++ Longest Subarray With Sum K.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+//  Most Optimal Appraoch for array containing negative elements also alongwith postitive and zeros
+
 int getLongestSubarray(vector<int>& nums, int k){
     map<int, int> preSumMap;
     int sum=0;
@@ -18,6 +20,28 @@ int getLongestSubarray(vector<int>& nums, int k){
         if(preSumMap.find(sum)==preSumMap.end()){
             preSumMap[sum]=i;
         }
+    }
+    return maxLen;
+
+}
+
+// Most Optimal Approach for array containing positive and zeros elements (not for negative elements)
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    int left=0,right=0;
+    long long sum=a[0];
+    int n=a.size();
+    int maxLen=0;
+    while(right<n){
+        while(left<=right && sum>k){
+            sum-=a[left];
+            left++;
+        }
+        if(sum==k){
+            maxLen=max(maxLen,right-left+1);
+        }
+        right++;
+        if(right<n) sum+=a[right];
     }
     return maxLen;
 
