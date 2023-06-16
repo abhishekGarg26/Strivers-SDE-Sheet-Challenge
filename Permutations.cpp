14561f2d3b92073b3e74b51d0691dfffa059cb58
Abhishek Garg <workwithabhigarg@gmail.com>
2023-06-16 11:02:19 +0530

    Permutations Using Recursion but not extra space

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,6 +1,8 @@
  #include<bits/stdc++.h>
- 
+
  using namespace std;
+
+ //Using Recursion and Extra Space
 
  void recurPermute(vector<int> &ds,vector<int> &nums,vector<vector<int>> &ans,int freq[]){
         if(ds.size()==nums.size()){
@@ -25,3 +27,27 @@
         recurPermute(ds,nums,ans,freq);
         return ans;
     }
+
+
+// Using Recursion but not extra space
+
+class Solution {
+public:
+    void recurPermute(int ind,vector<int> &nums,vector<vector<int>> &ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            recurPermute(ind+1,nums,ans); 
+            swap(nums[ind],nums[i]); 
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int ind=0;
+        recurPermute(ind,nums,ans);
+        return ans;
+    }
+};
