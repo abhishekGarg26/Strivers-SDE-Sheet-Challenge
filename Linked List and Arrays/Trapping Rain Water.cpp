3b215491074be68f2e49123b3731dd269551bcf6
Abhishek Garg <workwithabhigarg@gmail.com>
2023-06-28 00:37:59 +0530

    Trapping Rain Water Approach-2 solved

--- Linked List and Arrays/Trapping Rain Water.cpp
+++ Linked List and Arrays/Trapping Rain Water.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 class Solution {
 public:
+    // Approach-1
+
     int trap(vector<int>& height) {
         int n=height.size();
         int left=0,right=n-1;
@@ -23,4 +25,31 @@ public:
         return res;
         
     }
+
+    // Approach-2
+
+    long getTrappedWater(long *height, int n){
+    // Write your code here.
+        long pre[n];
+        long suf[n];
+        long maxl=height[0];
+        long maxr=height[n-1];
+        long cnt=0;
+        for(int i=0;i<n;i++){
+            if(height[i]>maxl){
+                maxl=height[i];
+            }
+            pre[i]=maxl;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(height[i]>maxr){
+                maxr=height[i];
+            }
+            suf[i]=maxr;
+        }
+        for(int i=0;i<n;i++){
+            cnt+=min(pre[i],suf[i])-height[i];
+        }
+        return cnt;
+}
 };
