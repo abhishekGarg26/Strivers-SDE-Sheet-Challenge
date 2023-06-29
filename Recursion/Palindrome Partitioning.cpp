6d4e223e061e1f42a53d597ce54b965bb81ee135
Abhishek Garg <workwithabhigarg@gmail.com>
2023-06-29 14:53:23 +0530

    Palindrome Partitioning solved

---
+++ Recursion/Palindrome Partitioning.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+class Solution {
+public:
+    bool isPalindrome(string s,int start,int end){
+        while(start<=end){
+            if(s[start++]!=s[end--]) return false;
+        }
+        return true;
+    }
+    void func(int ind,string s,vector<string> &path,vector<vector<string>> &res){
+        if(ind==s.size()){
+            res.push_back(path);
+            return;
+        }
+        for(int i=ind;i<s.size();i++){
+            if(isPalindrome(s,ind,i)){
+            path.push_back(s.substr(ind,i-ind+1));
+            func(i+1,s,path,res);
+            path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        string str="";
+        vector<vector<string>> res;
+        vector<string> path;
+        func(0,s,path,res);
+        return res;
+    }
+};
