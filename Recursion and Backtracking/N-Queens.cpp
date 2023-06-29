e040101aeb8a6ccb6a73d7d6929af01e2370dae2
Abhishek Garg <workwithabhigarg@gmail.com>
2023-06-30 01:54:02 +0530

    N-Queens Approach-2 solved

--- Recursion and Backtracking/N-Queens.cpp
+++ Recursion and Backtracking/N-Queens.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Solution {
 public:
+    // Approach-1 (Most Optimal)
     void solve(int col,vector<vector<string>> &ans,vector<string> &board,vector<int> &leftRow,vector<int> &lowerDiagonal,vector<int> &upperDiagonal,int n){
         if(col==n){
             ans.push_back(board);
@@ -34,6 +35,58 @@ public:
         }
         vector<int> leftRow(n,0), lowerDiagonal(2*n-1,0), upperDiagonal(2*n-1,0);
         solve(0,ans,board,leftRow,lowerDiagonal,upperDiagonal,n);
+        return ans;
+
+    }
+
+    // Approach-2
+
+     bool isSafe(int row,int col,vector<string> board,int n){
+        int duprow=row;
+        int dupcol=col;
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q') return false;
+            row--;
+            col--;
+        }
+        row=duprow;
+        col=dupcol;
+        while(col>=0){
+            if(board[row][col]=='Q') return false;
+            col--;
+        }
+        col=dupcol;
+        while(row<n && col>=0){
+            if(board[row][col]=='Q') return false;
+            row++;
+            col--;
+        }
+        return true;
+    }
+
+    void solve(int col,vector<vector<string>> &ans,vector<string> &board,int n){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++){
+                if(isSafe(row,col,board,n)){
+                board[row][col]='Q';
+                solve(col+1,ans,board,n);
+                board[row][col]='.';
+            }
+        }
+        return;
+
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        solve(0,ans,board,n);
         return ans;
 
     }
