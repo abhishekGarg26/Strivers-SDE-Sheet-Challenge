e685fa960e88e92ed38a5b639af696914a882325
Abhishek Garg <workwithabhigarg@gmail.com>
2023-07-01 18:41:21 +0530

    Find Nth Root Of M solved

---
+++ Binary Search/Find Nth Root Of M.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+int func(int n, int mid,int m){
+  long long ans=1;
+  for(int i=0;i<n;i++){
+    ans=ans*mid;
+    if(ans>m) return 2;
+  }
+  if(ans==m) return 1;
+  return 0;
+}
+int NthRoot(int n, int m) {
+  // Write your code here.
+  int low=1;
+  int high=m;
+  while(low<=high){
+    int mid=(low+high)/2;
+    int midN=func(n,mid,m);
+    if(midN==1) return mid;
+    else if(midN == 0) low=mid+1;
+    else high=mid-1;
+  }
+  return -1;
+
+}
