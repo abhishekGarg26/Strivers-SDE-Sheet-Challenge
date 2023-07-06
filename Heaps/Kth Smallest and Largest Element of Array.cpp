193af05d6bc80796f6362f6d05b83ec4c13c01dd
Abhishek Garg <workwithabhigarg@gmail.com>
2023-07-06 15:55:43 +0530

    kthSmallLarge Approach-2 solved

--- Heaps/Kth Smallest and Largest Element of Array.cpp
+++ Heaps/Kth Smallest and Largest Element of Array.cpp
@@ -17,3 +17,27 @@ vector<int> kthSmallLarge(vector<int> &nums, int n, int k)
         }
         return {minh.top(),pq.top()};
 }
+
+// Approach-2 (maintaining only k-sized heaps)
+
+vector<int> kthSmallLarge(vector<int> &nums, int n, int k)
+{
+	// Write your code here.
+		priority_queue<int> maxHeap;
+        priority_queue<int,vector<int>,greater<int>> minHeap;
+        for(int i=0;i<k;i++){
+            maxHeap.push(nums[i]);
+            minHeap.push(nums[i]);
+        }
+        for(int i=k;i<n;i++){
+            if(nums[i] < maxHeap.top()){
+                maxHeap.pop();
+                maxHeap.push(nums[i]);
+            }
+            if(nums[i] > minHeap.top()){
+                minHeap.pop();
+                minHeap.push(nums[i]);
+            }
+        }
+        return {maxHeap.top(),minHeap.top()};
+}
