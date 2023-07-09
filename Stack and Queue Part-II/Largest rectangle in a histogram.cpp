7f82e81cc0378f2ea40c8098d36147225cdda384
Abhishek Garg <workwithabhigarg@gmail.com>
2023-07-09 17:44:16 +0530

    Largest rectangle in a histogram solved

--- Stack and Queue Part-II/Largest rectangle in a histogram.cpp
+++ Stack and Queue Part-II/Largest rectangle in a histogram.cpp
@@ -33,3 +33,25 @@ using namespace std;
    }
    return maxA;
  }
+
+//  Appraoch-2
+
+ int largestRectangle(vector < int > & histo) {
+   // Write your code here.
+   stack<int> st;
+   int maxA=0;
+   int n=histo.size();
+   for(int i=0;i<=n;i++){
+     while(!st.empty() && (i==n || histo[st.top()]>=histo[i])){
+       int height=histo[st.top()];
+       st.pop();
+       int width;
+       if(st.empty()) width=i;
+       else width=i-st.top()-1;
+       maxA=max(maxA,width*height);
+     }
+     st.push(i);
+   }
+   return maxA;
+   
+ }
