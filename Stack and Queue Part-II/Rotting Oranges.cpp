a6b0e167a885f3e30ee495f69a45050d7d7e1e4d
Abhishek Garg <workwithabhigarg@gmail.com>
2023-07-10 20:40:03 +0530

    Rotting Oranges solved

---
+++ Stack and Queue Part-II/Rotting Oranges.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        queue<pair<pair<int,int>,int>> q;
+        int vis[m][n];
+        int cntFresh=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
+                }else{
+                    vis[i][j]=0;
+                }
+                if(grid[i][j]==1) cntFresh++;
+            }
+        }
+        int maxtime=0;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        int cnt=0;
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int time=q.front().second;
+            maxtime=max(maxtime,time);
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    q.push({{nrow,ncol},time+1});
+                    vis[nrow][ncol]=2;
+                    cnt++;
+                }
+            }
+        }
+        if(cnt!=cntFresh) return -1;
+        return maxtime;
+
+    }
+};
