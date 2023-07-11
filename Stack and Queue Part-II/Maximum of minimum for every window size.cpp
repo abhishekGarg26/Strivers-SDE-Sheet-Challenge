9fd053bee816142c9517f8ddd58f693ba7b61a01
Abhishek Garg <workwithabhigarg@gmail.com>
2023-07-11 15:52:28 +0530

    Maximum of minimum for every window size solved

--- Stack and Queue Part-II/Maximum of minimum for every window size.cpp
+++ Stack and Queue Part-II/Maximum of minimum for every window size.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+// Approach-1
+
 vector<int> maxMinWindow(vector<int> a, int n) {
     // Write your code here.
     stack<int> st;
@@ -35,6 +37,38 @@ vector<int> maxMinWindow(vector<int> a, int n) {
     for(int i=0;i<n;i++){
         int len=(right[i]-left[i]+1);
         ans[len]=max(ans[len],a[i]);
+    }
+    for(int i=n-1;i>0;i--){
+        ans[i]=max(ans[i],ans[i+1]);
+    }
+    for(int i=1;i<n+1;i++){
+        res.push_back(ans[i]);
+    }
+    return res;
+
+}
+
+// Appraoch-2
+
+#include <bits/stdc++.h> 
+vector<int> maxMinWindow(vector<int> a, int n) {
+    // Write your code here.
+    stack<int> st;
+    vector<int> res;
+    int ans[n+1];
+    for(int i=0;i<n+1;i++) ans[i]=INT_MIN;
+    for(int i=0;i<=n;i++){
+        while(!st.empty() && (i==n || a[st.top()]>=a[i])){
+            int rightSmall=i-1;
+            int leftSmall;
+            int el=a[st.top()];
+            st.pop();
+            if(st.empty()) leftSmall=0;
+            else leftSmall=st.top()+1;
+            int windowSize=rightSmall-leftSmall+1;
+            ans[windowSize]=max(ans[windowSize],el);
+        }
+        st.push(i);
     }
     for(int i=n-1;i>0;i--){
         ans[i]=max(ans[i],ans[i+1]);
