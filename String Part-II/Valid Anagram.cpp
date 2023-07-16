3d5a87abcdb907f14dcaf3a1d2d8aa45054ebbf8
Abhishek Garg <workwithabhigarg@gmail.com>
2023-07-16 21:53:26 +0530

    Valid Anagrams solved

--- String Part-II/Valid Anagram.cpp
+++ String Part-II/Valid Anagram.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+// Approach-1
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -11,3 +13,22 @@ public:
 
     }
 };
+
+// Approach-2 
+
+ bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        vector<int> arr(256,0);
+        for(int i=0;i<s.size();i++){
+            int index1=s[i];
+            int index2=t[i];
+            arr[index1]++;
+            arr[index2]--;
+        }
+        for(int i=0;i<s.size();i++){
+            int index=s[i];
+            if(arr[index]!=0) return false;
+        }
+        return true;
+
+    }
