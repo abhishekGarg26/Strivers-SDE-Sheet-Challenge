ec30c18442a1a4e6ac3ae6bf1ed4f62c7cb889de
Abhishek Garg <workwithabhigarg@gmail.com>
2023-07-26 16:12:09 +0530

    BFS of graph solved

--- Graph/BFS in Graph.cpp
+++ Graph/BFS in Graph.cpp
@@ -1,8 +1,65 @@
-#include <bits/stdc++.h> 
-
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-#include <bits/stdc++.h> 
+// } Driver Code Ends
+class Solution {
+  public:
+    // Approach-1 (When adjacency list is given)
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        int vis[V]={0};
+        vis[0]=1;
+        queue<int> q;
+        q.push(0);
+        vector<int> bfs;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            bfs.push_back(node);
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
+                }
+            
+        }
+        return bfs;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+// Approach-2 (When Edges are given)
+
 void BFS(int start,int vis[],vector<int> &ans,vector<int> adj[]){
         queue<int> q;
         q.push(start);
