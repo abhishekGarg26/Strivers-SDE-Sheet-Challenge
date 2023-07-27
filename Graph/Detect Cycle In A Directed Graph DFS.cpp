b2a6cf6d517472d95be2fecb6540b58d8c3b77d9
Abhishek Garg <workwithabhigarg@gmail.com>
2023-07-27 16:04:58 +0530

    Detect Cycle In A Directed Graph solved

---
+++ Graph/Detect Cycle In A Directed Graph DFS.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Approach -1 (Using Two Arrays vis & pathVis)
+
+bool dfs(int node,int vis[],int pathVis[],vector<int> adj[]){
+  vis[node]=1;
+  pathVis[node]=1;
+    // visit adjacent nodes
+  for(auto adjacentNode: adj[node]){
+    // when the node is not visited
+    if(!vis[adjacentNode]){
+      if(dfs(adjacentNode,vis,pathVis,adj)==true) return true;
+    }
+    // If the node has been previously visited but it has been visited in the same path
+    else if(pathVis[adjacentNode]){
+      return true;
+    }
+  }
+  pathVis[node]=0;
+  return false;
+}
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  // Write your code here.
+    vector<int> adj[n+1];
+    for(auto it: edges){
+        adj[it.first].push_back(it.second);
+    }
+    int vis[n+1]={0};
+    int pathVis[n+1]={0};
+    for(int i=1;i<n+1;i++){
+        if(!vis[i]){
+            if(dfs(i,vis,pathVis,adj)==true){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+// Approach -2 (Using only single vis array)
+
+bool dfs(int node,int vis[],vector<int> adj[]){
+  vis[node]=1;
+  for(auto adjacentNode: adj[node]){
+    if(!vis[adjacentNode]){
+      if(dfs(adjacentNode,vis,adj)==true) return true;
+    }else if(vis[adjacentNode]==1){
+      return true;
+    }
+  }
+  vis[node]=2;
+  return false;
+}
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  // Write your code here.
+    vector<int> adj[n+1];
+    for(auto it: edges){
+        adj[it.first].push_back(it.second);
+    }
+    int vis[n+1]={0};
+    for(int i=1;i<n+1;i++){
+        if(!vis[i]){
+            if(dfs(i,vis,adj)==true){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
