4487f18538f18fee25e6283f5dd9b59bbaf6483a
Abhishek Garg <workwithabhigarg@gmail.com>
2023-08-05 23:53:55 +0530

    Bellman-Ford Algorithm solved

--- Graph Part-II/Bellman-Ford Algo.cpp
+++ Graph Part-II/Bellman-Ford Algo.cpp
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-int bellmonFord(int V, int m, int S, int dest, vector<vector<int>> &edges) {
-    // Write your code here.
-        vector<int> dist(V+1, 1e9);
-		dist[S] = 0;
-		for (int i = 1; i <= V ; i++) {
-			for (int j=0;j<m;j++) {
-				int u = edges[j][0];
-				int v = edges[j][1];
-				int wt = edges[j][2];
-				if (dist[u] != 1e9 && dist[u] + wt < dist[v]) {
-					dist[v] = dist[u] + wt;
-				}
-			}
-		}
-        bool flag=0;
-		// Nth relaxation to check negative cycle
-		for (int j=0;j<m;j++) {
-			int u = edges[j][0];
-			int v = edges[j][1];
-			int wt = edges[j][2];
-			if (dist[u] != 1e9 && dist[u] + wt < dist[v]) {
-				flag=1;
-			}
-		}
-        if(flag==0) return dist[dest];
-
-		return -1;
+vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        // Code here
+        vector<int> dist(V,1e8);
+        dist[S]=0;
+        // TC ->  V x E
+        for(int i=0;i<V-1;i++){
+            for(auto it:edges){
+                int u=it[0];
+                int v=it[1];
+                int wt=it[2];
+                if(dist[u]!=1e8 && dist[u]+wt < dist[v]){
+                    dist[v]=dist[u]+wt;
+                }
+            }
+        }
+        // Nth relaxation to check negative cycle
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
+            if(dist[u]!=1e8 && dist[u]+wt < dist[v]){
+                return {-1};
+            }
+        }
+        return dist;
 }
