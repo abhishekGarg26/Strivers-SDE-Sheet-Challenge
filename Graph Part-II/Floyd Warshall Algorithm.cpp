c6ab6a7775c5eb228c92a02b322751b1f4250e8f
Abhishek Garg <workwithabhigarg@gmail.com>
2023-08-06 14:26:03 +0530

    Floyd Warshall solved

--- Graph Part-II/Floyd Warshall Algorithm.cpp
+++ Graph Part-II/Floyd Warshall Algorithm.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+// Approach-1 (When only positive weights are allowed)
+
 void shortest_distance(vector<vector<int>>&matrix){
 	    // Code here
 	    int n=matrix.size();
@@ -33,3 +35,34 @@ void shortest_distance(vector<vector<int>>&matrix){
 	        }
 	    }
 	}
+
+    // Approach-2 (When negative weights are also allowed)
+
+    int floydWarshall(int n, int m, int src, int dest, vector<vector<int>> &edges) {
+    // Write your code here.
+		vector<vector<int>> matrix(n+1,vector<int>(n+1,1e9));
+		for(auto it: edges){
+			int u=it[0];
+			int v=it[1];
+			int wt=it[2];
+			matrix[u][v]=wt;
+
+		}
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=n;j++){
+                // -1 represents a node which is not reachable directly
+	            if(i==j) matrix[i][j]=0;
+	        }
+	    }
+	    for(int k=1;k<=n;k++){
+	        for(int i=1;i<=n;i++){
+	            for(int j=1;j<=n;j++){
+	                matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+	            }
+	        }
+	    }
+        // If value of weight go out of given range of weight then no path exists to dest node so return 1e9
+		 if(matrix[src][dest]>1e5) return 1e9;
+	   
+	    return matrix[src][dest];
+}
