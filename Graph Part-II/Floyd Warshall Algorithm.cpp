adf1b10f675815d3d7dba14afb034799e33d6dfa
Abhishek Garg <workwithabhigarg@gmail.com>
2023-08-06 12:54:12 +0530

    Floyd Warshall solved

--- Graph Part-II/Floyd Warshall Algorithm.cpp
+++ Graph Part-II/Floyd Warshall Algorithm.cpp
@@ -1,31 +1,35 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-int floydWarshall(int V, int m, int S, int dest, vector<vector<int>> &edges) {
-    // Write your code here.
-    vector<int> dist(V+1, 1e9);
-		dist[S] = 0;
-		for (int i = 1; i <= V ; i++) {
-			for (int j=0;j<m;j++) {
-				int u = edges[j][0];
-				int v = edges[j][1];
-				int wt = edges[j][2];
-				if (dist[u] != 1e9 && dist[u] + wt < dist[v]) {
-					dist[v] = dist[u] + wt;
-				}
-			}
-		}
-        bool flag=0;
-		// Nth relaxation to check negative cycle
-		for (int j=0;j<m;j++) {
-			int u = edges[j][0];
-			int v = edges[j][1];
-			int wt = edges[j][2];
-			if (dist[u] != 1e9 && dist[u] + wt < dist[v]) {
-				flag=1;
-			}
-		}
-        if(flag==0) return dist[dest];
+void shortest_distance(vector<vector<int>>&matrix){
+	    // Code here
+	    int n=matrix.size();
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+                // -1 represents a node which is not reachable directly
+	            if(matrix[i][j]==-1){
+	                matrix[i][j]=1e9;
+	            }
+	            if(i==j) matrix[i][j]=0;
+	        }
+	    }
+	    for(int k=0;k<n;k++){
+	        for(int i=0;i<n;i++){
+	            for(int j=0;j<n;j++){
+	                matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+	            }
+	        }
+	    }
+	   // For detecting negative cycle
+	    for(int i=0;i<n;i++){
+	        if(matrix[i][i]<0) return;
+	    }
 
-		return -1;
-}
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            if(matrix[i][j]==1e9){
+	                matrix[i][j]=-1;
+	            }
+	        }
+	    }
+	}
