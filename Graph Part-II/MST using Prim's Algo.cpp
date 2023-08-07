e6410794e202d6e5f21f55860d17bdd0f12a2627
Abhishek Garg <workwithabhigarg@gmail.com>
2023-08-07 20:21:09 +0530

    Minimum Spanning Tree using Prim's Algo solved

--- Graph Part-II/MST using Prim's Algo.cpp
+++ Graph Part-II/MST using Prim's Algo.cpp
@@ -1,49 +1,78 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
+// Approach-1 (When only mstSum is asked for graph consisting of nodes from 0 to N)
+int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        priority_queue<pair<int,int>,
+        vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> vis(V,0);
+        // {wt,node}
+        pq.push({0,0});
+        int sum=0;
+        // ElogE + ElogE
+        while(!pq.empty()){
+            // logE
+            auto it=pq.top();
+            int edgeWt=it.first;
+            int node=it.second;
+            pq.pop();
+            if(vis[node]==1) continue;
+            // add it to the mst
+            vis[node]=1;
+            sum+=edgeWt;
+            // ElogE
+            for(auto it:adj[node]){
+                int adjNode=it[0];
+                int wt=it[1];
+                if(!vis[adjNode]){
+                    pq.push({wt,adjNode});
+                }
+            }
+        }
+        return sum;
+    }
+
+// Approach-2 {Finding both mstSum and MST edges with weights}    
+// Priority Queue storing {weight,{node,parent}} where nodes = 1 to N
+vector<pair<pair<int, int>, int>> calculatePrimsMST(int V, int m, vector<pair<pair<int, int>, int>> &g)
 {
     // Write your code here.
-    unordered_map<int,list<pair<int,int>>> adj;
-    for(int i=0;i<g.size();i++){
-        int u=g[i].first.first;
-        int v=g[i].first.second;
-        int w=g[i].second;
-        adj[u].push_back({v,w});
-        adj[v].push_back({u,w});
-    }
-    vector<int> key(n+1);
-    vector<bool> mst(n+1);
-    vector<int> parent(n+1);
-    for(int i=0;i<=n;i++){
-        key[i]=INT_MAX;
-        parent[i]=-1;
-        mst[i]=false;
-    }
-    key[1]=0;
-    parent[1]=-1;
-    for(int i=1;i<n;i++){
-        int mini=INT_MAX;
-        int u;
-        for(int v=1;v<=n;v++){
-            if(mst[v]==false && key[v]<mini){
-                u=v;
-                mini=key[v];
+        vector<vector<int>> adj[V+1];
+        for(auto it:g){
+            int u=it.first.first;
+            int v=it.first.second;
+            int wt=it.second;
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
+        }
+        priority_queue<pair<int,pair<int,int>>,
+        vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        pq.push({1,{1,-1}});
+        int sum=0;
+        // MST storing {{parent,node},weight}
+        vector<pair<pair<int,int>,int>> mst;
+        vector<int> vis(V+1,0);
+        while(!pq.empty()){
+            auto it=pq.top();
+            int wt=it.first;
+            int node=it.second.first;
+            int parent=it.second.second;
+            pq.pop();
+            if(vis[node]==1) continue;
+            if(parent!=-1){
+                mst.push_back({{parent,node},wt});
+            }
+            vis[node]=1;
+            sum+=wt;
+            for(auto it:adj[node]){
+                int adjNode=it[0];
+                int edW=it[1];
+                if(!vis[adjNode]){
+                    pq.push({edW,{adjNode,node}});
+                }
             }
         }
-        mst[u]=true;
-        for(auto it:adj[u]){
-            int v=it.first;
-            int w=it.second;
-            if(mst[v]==false && w<key[v]){
-                parent[v]=u;
-                key[v]=w;
-            }
-        }
-    }
-    vector<pair<pair<int,int>,int>> result;
-        for(int i=2;i<=n;i++){
-            result.push_back({{parent[i],i},key[i]});
-        }
-        return result;
+        return mst;
 }
