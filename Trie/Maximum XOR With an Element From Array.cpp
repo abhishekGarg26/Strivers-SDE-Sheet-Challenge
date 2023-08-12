2df06164da7e1f22d82f3dea32843513f2fc062d
Abhishek Garg <workwithabhigarg@gmail.com>
2023-08-12 09:53:16 +0530

    Maximum XOR With an Element From Array solved

--- Trie/Maximum XOR With an Element From Array.cpp
+++ Trie/Maximum XOR With an Element From Array.cpp
@@ -1,80 +1,83 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct Node{
+    Node* links[2];
 
-struct Node {
-  Node * links[2];
-
-  bool containsKey(int ind) {
-    return (links[ind] != NULL);
-  }
-  Node * get(int ind) {
-    return links[ind];
-  }
-  void put(int ind, Node * node) {
-    links[ind] = node;
-  }
-};
-class Trie {
-  private: Node * root;
-  public:
-    Trie() {
-      root = new Node();
+    bool containsKey(int bit){
+        return (links[bit]!=NULL);
     }
 
-  public:
-    void insert(int num) {
-      Node * node = root;
-      for (int i = 31; i >= 0; i--) {
-        int bit = (num >> i) & 1;
-        if (!node -> containsKey(bit)) {
-          node -> put(bit, new Node());
-        }
-        node = node -> get(bit);
-      }
+    Node* get(int bit){
+        return links[bit];
     }
-  public:
-    int findMax(int num) {
-      Node * node = root;
-      int maxNum = 0;
-      for (int i = 31; i >= 0; i--) {
-        int bit = (num >> i) & 1;
-        if (node -> containsKey(!bit)) {
-          maxNum = maxNum | (1 << i);
-          node = node -> get(!bit);
-        } else {
-          node = node -> get(bit);
-        }
-      }
-      return maxNum;
+
+    void put(int bit,Node* node){
+        links[bit]=node;
     }
 };
-vector<int> maxXorQueries(vector<int> &arr, vector<vector<int>> &queries){
-	//	Write your coode here.
-	vector < int > ans(queries.size(), 0);
-  vector < pair < int, pair < int, int >>> offlineQueries;
-  sort(arr.begin(), arr.end());
-  int index = 0;
-  for (auto & it: queries) {
-    offlineQueries.push_back({
-      it[1],
-      {
-        it[0],
-        index++
-      }
-    });
-  }
-  sort(offlineQueries.begin(), offlineQueries.end());
-  int i = 0;
-  int n = arr.size();
-  Trie trie;
 
-  for (auto & it: offlineQueries) {
-    while (i < n && arr[i] <= it.first) {
-      trie.insert(arr[i]);
-      i++;
+class Trie{
+    private: 
+        Node* root;
+    public:
+        Trie(){
+            root=new Node();
+        }
+        void insert(int num){
+            Node* node=root;
+            for(int i=31;i>=0;i--){
+                int bit= (num>>i) & 1;
+                if(!node->containsKey(bit)){
+                    node->put(bit,new Node());
+                }
+                node=node->get(bit);
+            }
+        }
+        int getMax(int num){
+            Node* node=root;
+            int maxNum=0;
+            for(int i=31;i>=0;i--){
+                int bit= (num>>i) & 1;
+                if(node->containsKey(1-bit)){
+                    maxNum= maxNum | (1<<i);
+                    node=node->get(1-bit);
+                }else{
+                    node=node->get(bit);
+                }
+            }
+            return maxNum;
+        }
+
+};
+class Solution {
+public:
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        // O(nlogn)
+        sort(nums.begin(),nums.end());
+        vector<pair<int , pair<int,int>>> oQ;
+        int q=queries.size();
+        // O(q)
+        for(int i=0;i<q;i++){
+            oQ.push_back({queries[i][1],{queries[i][0],i}});
+        }
+        // O(qlogq)
+        sort(oQ.begin(),oQ.end());
+        vector<int> ans(q,0);
+        int ind=0;
+        int n=nums.size();
+        Trie trie;
+        // O(q*34 + n*34)
+        for(int i=0;i<q;i++){
+            int ai= oQ[i].first;
+            int xi= oQ[i].second.first;
+            int qInd= oQ[i].second.second;
+            while(ind < n && nums[ind]<=ai){
+                trie.insert(nums[ind]);
+                ind++;
+            }
+            if(ind ==0 ) ans[qInd]=-1;
+            else ans[qInd] = trie.getMax(xi);
+        }
+        return ans;
     }
-    if (i != 0) ans[it.second.second] = trie.findMax(it.second.first);
-    else ans[it.second.second] = -1;
-  }
-  return ans;
-}
+};
