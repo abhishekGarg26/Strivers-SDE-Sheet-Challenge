ff6c96a183eea963bb3cdce5c1594e84ef1c4404
Abhishek Garg <workwithabhigarg@gmail.com>
2023-08-21 11:31:18 +0530

    Maximum sum increasing subsequence solved

--- Dynamic Programming/Maximum sum increasing subsequence.cpp
+++ Dynamic Programming/Maximum sum increasing subsequence.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+// Approach (Using Tabulation in algorithm as used in LIS & we can also trace it using it)
+// It can also be solved with all the approaches used in LIS
 int maxIncreasingDumbbellsSum(vector<int> &arr, int n)
 {
 	// Write your code here
-	
     int i, j, max = 0; 
     int msis[n]; 
     
